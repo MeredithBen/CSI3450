2a9a9c377f2b044acfe55e0373150c59b05d42ea
Charles Kalajian <55409585+ckal48@users.noreply.github.com>
2020-12-12 14:19:55 -0500

    Add files via upload

--- playlists.js
+++ playlists.js
@@ -9,13 +9,31 @@ router.get('/', (req, res) => {
     mysqlConnection.query('SELECT * FROM playlists LIMIT 100', (err, rows) => {
 
     console.log(rows);
-    res.render('playlists', {
-        playlists : rows
-        });
-     });
-  })
+});
 
-  router.post('/', function(req, res) {
+    let userIDQuery = "SELECT user_id FROM user WHERE user_name = " + "'" + req.session.username + "'";
+    mysqlConnection.query(userIDQuery, (err, results) => {
+        if (err) throw err;
+        let userID = results[0].user_id;
+
+        let playlistQuery = "SELECT song_name, name1.plist_name FROM song " +
+            "JOIN(SELECT song_id, name2.plist_name FROM song_playlist_link " +
+            "JOIN(SELECT name3.plist_id, name3.plist_name FROM user_playlist_link " +
+            "JOIN(SELECT * FROM playlists) AS name3 " +
+            "ON user_playlist_link.plist_id = name3.plist_id " +
+            "WHERE user_id = " + userID + ") AS name2 " +
+            "ON name2.plist_id = song_playlist_link.plist_id) AS name1 " +
+            "ON name1.song_id = song.song_id"
+        mysqlConnection.query(playlistQuery, (err, result) => {
+            if (err) throw err;
+            res.render('playlists', {
+                playlists : result
+            }); 
+        });
+    });   
+})
+  
+  router.post('/create', function(req, res) {
     var playlistName = req.body.createPlaylist;
     console.log(playlistName);
 
@@ -58,6 +76,5 @@ router.get('/', (req, res) => {
     })
 })
 
+
 module.exports = router; 
-
-
